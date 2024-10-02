cee25ac627aa4117b12a2a84360a35f17c66ea3a
yumemizu5 <149136184+yumemizu5@users.noreply.github.com>
2024-10-02 15:07:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-# 以下を「app.py」に書き込み
 import streamlit as st
 import openai
-
-
+from gtts import gTTS
+import os
+from io import BytesIO
 
 # パスワードを設定
 correct_password = st.secrets.mieai_pw.correct_password
@@ -14,7 +14,7 @@ password = st.text_input("パスワードを入力してください", type="pas
 if password == correct_password:
 
     openai.api_key = st.secrets.OpenAIAPI.openai_api_key
-    
+
     system_prompt = """
     あなたは優秀な人の悩みを解決するコーチです。
     悩みに対して質問を行ったりして深堀も行ってください。
@@ -26,46 +26,54 @@ if password == correct_password:
     * 科学
     * 歴史
     """
-    
+
     # st.session_stateを使いメッセージのやりとりを保存
     if "messages" not in st.session_state:
         st.session_state["messages"] = [
             {"role": "system", "content": system_prompt}
-            ]
-    
+        ]
+
     # チャットボットとやりとりする関数
     def communicate():
         messages = st.session_state["messages"]
-    
+
         user_message = {"role": "user", "content": st.session_state["user_input"]}
         messages.append(user_message)
-    
+
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages
         )
-    
+
         bot_message = response["choices"][0]["message"]
         messages.append(bot_message)
-    
+
         st.session_state["user_input"] = ""  # 入力欄を消去
-    
-    
+
+        # ボットの応答メッセージを音声に変換
+        tts = gTTS(bot_message["content"], lang='ja')  # 日本語対応
+        tts_file = BytesIO()
+        tts.save(tts_file)
+        tts_file.seek(0)
+
+        # 音声再生
+        st.audio(tts_file)
+
     # ユーザーインターフェイスの構築
     st.title("「みえAi」コーチングボット")
     st.image("mieai.png")
     st.write("悩み事は何ですか？")
-    
+
     user_input = st.text_input("悩み事を下に入力してください。", key="user_input", on_change=communicate)
-    
+
     if st.session_state["messages"]:
         messages = st.session_state["messages"]
-    
+
         for message in reversed(messages[1:]):  # 直近のメッセージを上に
             speaker = "🙂"
-            if message["role"]=="assistant":
-                speaker="🤖"
-    
+            if message["role"] == "assistant":
+                speaker = "🤖"
+
             st.write(speaker + ": " + message["content"])
 
 else:
